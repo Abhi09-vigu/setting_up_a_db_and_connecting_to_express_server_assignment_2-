b1a9ec310cb7ded81544f10f0cc5a3d36ee14eba
Abhi <abhivignesh27@gmail.com>
2025-02-22 10:09:06 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const { error } = require('console');
+require('dotenv').config()
 const app = express();
 const port = 3010;
-
 app.use(express.static('static'));
+const User = require('./schema');
+app.use(express.json())
+
+mongoose.connect(process.env.mongodb)
+.then((res)=>{
+   console.log("Connected")
+})
+.catch((error)=>{
+  console.log(error)
+});
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+app.post('/api/users',async(req,res)=>{
+  try {
+    let user=new User(req.body)
+    await user.save()
+    
+    res.status(200).json("user created")
+  } catch (error) {
+    if(error.name=="ValidationError"){
+      return res.status(400).json("mongodb validation failed check your key names")
+    }
+    res.status(500).json({msg:error.message})
+  }
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
